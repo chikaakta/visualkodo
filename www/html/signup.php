8f34fc1aa78c8267550e31b0b91b33076da99df8
chikaaktac <guillaumecadou@yahoo.fr>
2020-04-22 11:48:25 +0200

    signup add gender update

--- www/html/signup.php
+++ www/html/signup.php
@@ -3,36 +3,41 @@
     include '../api/connexionPost.php';
     include '../api/utf8.php';
 
-    if( $_POST['username'] && $_POST['password'] && $_POST['email'] && $_POST['lastname'] && $_POST['firstname']){
+    if( $_POST['username'] && $_POST['password'] && $_POST['email'] && $_POST['lastname'] && $_POST['firstname'] && $_POST['gender']){
 
-
-        $data = array();
-        $email = $_POST['email'];
-        $pwd = $_POST['password'];
-        $password = password_hash($pwd, PASSWORD_DEFAULT);
-        $sql = "SELECT * FROM users WHERE email='{$email}'";
-        $table_data = $mysql->query($sql);
-
-        while($row = $table_data->fetch_array(MYSQLI_ASSOC)){
-            $data[] = $row;
-        }
-
-        if(count($data) > 0){
-
-            $responce["MESSAGE"] = "EMAIL ALREADY REGISTER ARE YOU BRITISH ?";
-            $responce["STATUS"] = 418;
+        if($_POST['gender'] = 'other'){
+            $responce["MESSAGE"] = "GENDER CANNOT BE UNDEFINED";
+            $responce["STATUS"] = 400;
         }
         else{
+            $data = array();
+            $email = $_POST['email'];
+            $pwd = $_POST['password'];
+            $password = password_hash($pwd, PASSWORD_DEFAULT);
+            $sql = "SELECT * FROM users WHERE email='{$email}'";
+            $table_data = $mysql->query($sql);
 
-            $sql = "INSERT INTO users(email,username,password,lastname,firstname) VALUES ('{$_POST['email']}','{$_POST['username']}','{$password}','{$_POST['lastname']}','{$_POST['firstname']}');";
-            if($mysql->query($sql)){
+            while($row = $table_data->fetch_array(MYSQLI_ASSOC)){
+                $data[] = $row;
+            }
 
-                $responce["MESSAGE"] = "USER CREATED SUCEFFULY";
-                $responce["STATUS"] = 200;
+            if(count($data) > 0){
+
+                $responce["MESSAGE"] = "EMAIL ALREADY REGISTER";
+                $responce["STATUS"] = 400;
             }
             else{
-                $responce["MESSAGE"] = "QUERRY ERROR";
-                $responce["STATUS"] = 400;
+
+
+                $sql = "INSERT INTO users(email,username,password,lastname,firstname) VALUES ('{$_POST['email']}','{$_POST['username']}','{$password}','{$_POST['lastname']}','{$_POST['firstname']}');";
+                if($mysql->query($sql)){
+                    $responce["MESSAGE"] = "USER CREATED SUCEFFULY";
+                    $responce["STATUS"] = 200;
+                }
+                else{
+                    $responce["MESSAGE"] = "QUERRY ERROR";
+                    $responce["STATUS"] = 400;
+                }
             }
         }
     }
@@ -41,5 +46,7 @@
         $responce["MESSAGE"] = "INVALID REQUEST";
         $responce["STATUS"] = 400;
     }
+    
+    
     echo json_encode($responce);
 ?>
